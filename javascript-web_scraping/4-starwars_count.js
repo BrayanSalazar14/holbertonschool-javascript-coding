3c8196542c8ffd3e99c47b53ffa61cb2125e4865
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-27 14:29:58 -0500

    new task v1

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const request = require('request');
+request(url, (err, response, body) => {
+  if (err) throw err;
+  const data = JSON.parse(body).results;
+  const wedgeAntilles = 'https://swapi-api.hbtn.io/api/people/18/';
+  let count = 0;
+  for (const property of data) {
+    if (property.characters.includes(wedgeAntilles)) count += 1;
+  }
+  console.log(count);
+});
