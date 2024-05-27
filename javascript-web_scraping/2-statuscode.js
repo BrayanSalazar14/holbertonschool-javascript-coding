656d85b0d2e74886cd2c94c589031a6a4ecbcc83
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-27 12:31:52 -0500

    semistandard

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 const args = process.argv.slice(2);
 const request = require('request');
 request
-.get(args[0])
-.on('response', (response) => {
+  .get(args[0])
+  .on('response', (response) => {
     console.log('code:', response.statusCode);
-});
+  });
