05c4d7bb8d9fcacfd2595025048aea0af6f8f0a1
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-27 12:03:25 -0500

    semistandard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 const arg = process.argv[2];
 fs.readFile(arg, (err, data) => {
   if (err) throw err;
-    console.log(data.toString());
+  console.log(data.toString());
 });
