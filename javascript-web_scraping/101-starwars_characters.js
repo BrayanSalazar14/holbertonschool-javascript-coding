ed39f8f111c46d71fe8b694dfd156889ec861cd8
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-29 16:53:50 -0500

    new task

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -5,11 +5,13 @@ const request = require('request');
 request(url, (err, response, body) => {
   if (err) throw err;
   const dataApi = JSON.parse(body).characters;
+  const arr = []
   for (const data of dataApi) {
     request(data, (err, response, body) => {
       if (err) throw err;
       const character = JSON.parse(body).name;
-      console.log(character);
+      arr.push(character);
     });
   }
+  for (const i of arr) console.log(i)
 });
