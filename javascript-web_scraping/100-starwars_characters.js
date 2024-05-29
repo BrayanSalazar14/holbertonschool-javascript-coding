ecb252c34a1eb7c8aef50f4573fbc4896438e633
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-29 16:38:34 -0500

    new task v1

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 const request = require('request');
 request(url, (err, response, body) => {
-    if (err) throw err;
-    const dataApi = JSON.parse(body).characters;
-    dataApi.filter(property => {
-        request(property, (err, response, body) => {
-            if (err) throw err;
-            console.log(JSON.parse(body).name)
-        });
+  if (err) throw err;
+  const dataApi = JSON.parse(body).characters;
+  dataApi.forEach(property => {
+    request(property, (err, response, body) => {
+      if (err) throw err;
+      const character = JSON.parse(body).name;
+      console.log(character);
     });
-})
+  });
+});
