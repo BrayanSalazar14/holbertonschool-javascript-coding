5aeefda49c9866b90d9a436f22d7b3018b390149
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-06-05 16:39:57 -0500

    hot fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,8 @@
 console.log('Welcome to Holberton School, what is your name?');
+
 process.stdin.on('data', (name) => {
-  console.log(`Your name is: ${name.toString().trim()}`);
+  const trimmedName = name.toString().trim();
+  console.log(`Your name is: ${trimmedName}`);
   console.log('This important software is now closing');
   process.exit();
 });
