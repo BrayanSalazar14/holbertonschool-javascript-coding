334944bbc0828e69ae17dab68d0d4fc20076dac3
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-06-05 16:35:25 -0500

    v2

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,6 @@
-const readline = require('node:readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  rl.close();
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (name) => {
+  console.log(`Your name is: ${name.toString().trim()}`);
+  console.log('This important software is now closing');
+  process.exit();
 });
