d0531f4f665b1e39e4b29d2b1666a1034ee8f9a2
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-06-07 00:24:16 -0500

    new v

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 const port = 1245;
 
-function cvsToJson(file) {
+function csvToJson(file) {
   try {
     const data = fs.readFileSync(file, 'utf-8');
     const jsonData = [];
-    const rows = data.split('\n');
+    const rows = data.trim().split('\n');
     const headers = rows[0].split(',');
 
     for (let i = 1; i < rows.length; i += 1) {
@@ -16,8 +16,8 @@ function cvsToJson(file) {
       const object = {};
 
       for (let j = 0; j < headers.length; j += 1) {
-        const key = headers[j];
-        const value = values[j];
+        const key = headers[j].trim();
+        const value = values[j].trim();
         object[key] = value;
       }
       jsonData.push(object);
@@ -36,12 +36,12 @@ app.get('/', (req, res) => {
 app.get('/students', (req, res) => {
   const response = ['This is the list of our students'];
   try {
-    const jsonData = cvsToJson(process.argv[2]);
+    const jsonData = csvToJson(process.argv[2]);
     const studentsCs = [];
     const studentsSwe = [];
-    jsonData.forEach((students) => {
-      if (students.field === 'CS') studentsCs.push(students.firstname);
-      else if (students.field === 'SWE') studentsSwe.push(students.firstname);
+    jsonData.forEach((student) => {
+      if (student.field === 'CS') studentsCs.push(student.firstname);
+      else if (student.field === 'SWE') studentsSwe.push(student.firstname);
     });
     response.push(`Number of students: ${jsonData.length}`);
     response.push(`Number of students in CS: ${studentsCs.length}. List: ${studentsCs.join(', ')}`);
